7e328fa1406c64c2e752e0498bff8c3bfe3708d3
Victor Dods <victor.dods@gmail.com>
2017-08-28 12:45:21 -0700

    Refactored web client to have no state, but rather only issue raw transactional requests of the web server.

--- web/client/client.js
+++ web/client/client.js
@@ -76,82 +76,6 @@ angular
         });
     };
 
-    $scope.current_account_name     = null;
-    $scope.account_context_d        = {};
-
-    $scope.make_account_context     = function (account_name) {
-        $scope.account_context_d[account_name] = {
-            parent                          : $scope,
-            account_name                    : account_name,
-            balance                         : null,
-            create_account__account_name    : null,
-            create_account__initial_balance : null,
-            delete_account__account_name    : null,
-            transfer__from_account_name     : null,
-            transfer__to_account_name       : null,
-            transfer__amount                : null,
-            query_balance__account_name     : null
-        };
-    };
-    $scope.recompile_account_panel = function (account_name) {
-        const account_panel = angular.element(document.getElementById('account_panel'));
-        account_panel.attr('context', 'account_context_d["'+account_name+'"]');
-        // This nonsense updates the angularjs state based on the changes to the 'context' attribute.
-        account_panel.injector().invoke(function($compile){
-            $compile(account_panel)(account_panel.scope());
-        });
-    }
-    $scope.bootstrap_account = function (account_name) {
-        $scope.append_to_log('bootstrap_account("'+account_name+'");');
-
-        console.assert(!(account_name in Object.keys($scope.account_context_d)), 'Account "'+account_name+'" already exists');
-        $scope.make_account_context(account_name);
-
-        $scope.current_account_name = account_name;
-        const account_context       = $scope.account_context_d[account_name];
-        account_context.balance     = 'N/A';
-
-        $scope.recompile_account_panel(account_name);
-    };
-    $scope.switch_to_account = function (account_name) {
-        $scope.append_to_log('switch_account_panel_to("'+account_name+'");');
-
-        if (!(account_name in Object.keys($scope.account_context_d))) {
-            $scope.make_account_context(account_name);
-        }
-
-        $scope.current_account_name = account_name;
-        const account_context       = $scope.account_context_d[account_name];
-//         // Clear old state until the new state is retrieved.
-//         account_context.balance = '';
-
-        let promise;
-        // Admin doesn't actually have an account in the ledger state, so don't query for it.
-        if (account_name == 'Admin') {
-            // Make up the response packet that would otherwise be queried
-            promise = Promise.resolve({
-                response:{
-                    data:'N/A'
-                }
-            });
-        }
-        // Others have accounts in the ledger state, so query the balance.
-        else {
-            promise = $scope.query_balance(account_name);
-        }
-        promise
-        .then(response => {
-            $scope.append_to_log('response: ' + JSON.stringify(response));
-            account_context.balance = response.response.data;
-            $scope.recompile_account_panel(account_name);
-            $scope.input_account_name = '';
-        })
-        .catch(err => {
-            $scope.append_to_log('error: no account named "' + account_name + '" found (error was ' + err + ').');
-            $scope.input_account_name = '';
-        });
-    };
-
     $scope.create_account = function (invoking_user_name, account_name, initial_balance) {
         return $scope.post('create_account', {invoking_user_name:invoking_user_name, account_name:account_name, initial_balance:initial_balance});
     };
@@ -164,17 +88,24 @@ angular
     $scope.query_balance = function (invoking_user_name, account_name) {
         return $scope.get('query_balance', {invoking_user_name:invoking_user_name, account_name:account_name});
     };
+    $scope.clear_input_fields = function () {
+        $scope.create_account__invoking_user_name = null;
+        $scope.create_account__account_name = null;
+        $scope.create_account__initial_balance = null;
+
+        $scope.delete_account__invoking_user_name = null;
+        $scope.delete_account__account_name = null;
+
+        $scope.transfer__invoking_user_name = null;
+        $scope.transfer__from_account_name = null;
+        $scope.transfer__to_account_name = null;
+        $scope.transfer__amount = null;
+
+        $scope.query_balance__invoking_user_name = null;
+        $scope.query_balance__account_name = null;
+    }
 
     $timeout(() => {
-        $scope.append_to_log('testing');
-        $scope.bootstrap_account('Admin');
+        $scope.append_to_log('Web client started');
     });
-}])
-.directive('accountPanel', () => {
-    return {
-        scope: {
-            context: '=context'
-        },
-        templateUrl: 'partials/account_panel.html'
-    };
-});
+}]);
